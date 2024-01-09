49c9b5fdd9945a5dbcfc74b9506ba4fa0e8d895c
Oleg <olegpringudz@gmail.com>
2024-01-09 07:24:05 +0200

    GGGG

---
+++ js/018 script.js
@@ -0,0 +1,64 @@
+/* Задание на урок:
+
+1) Первую часть задания повторить по уроку
+
+2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
+false - выводит в консоль главный объект программы
+
+3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
+"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
+genres
+
+P.S. Функции вызывать не обязательно*/
+
+'use strict';
+
+let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '0');
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+}
+
+let question, answer;
+
+for (let i = 0; i < numberOfFilms; i++) {
+    do {
+        question = prompt('Один из последних просмотренных фильмов?');
+        answer = prompt('На сколько оцените его?');
+    } while (question == null || question == '' || answer == null || answer == '' || question.length > 50 || Number.isInteger(answer))
+
+    personalMovieDB.movies[question] = answer;
+
+}
+
+if (personalMovieDB.count < 10) {
+    alert("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+    alert("Вы классический зритель");
+} else if (personalMovieDB.count > 30) {
+    alert("Вы киноман");
+} else {
+    alert("Произошла ошибка");
+}
+
+
+function showMyDB() {
+    if (personalMovieDB.privat === false) {
+        console.log(personalMovieDB);
+    }
+}
+
+
+function writeYourGenres() {
+    for (let i = 0; i < 3; i++) {
+        personalMovieDB.genres.push(prompt(`Ваш любимый жанр под номером ${i}`));
+    }
+}
+
+writeYourGenres();
+
+showMyDB();
